e14c46b174213e7670c76f55ba7dddfff682eaac
bocar <bocaralhassanwane@gmail.com>
2021-05-24 11:08:00 -0400

    Add test for profile route

---
+++ routes/members/profile.test.js
@@ -0,0 +1,17 @@
+const { request } = require("../../__test__/testData/common.data");
+const { profileTestUser } = require("../../__test__/testData/users.data");
+
+describe("Test /profile", () => {
+  beforeAll(async () => {
+    await loadUser(profileTestUser);
+  });
+  afterAll(() => {
+    removeUser(profileTestUser.email);
+  });
+  describe("GET /profile", () => {
+    it("Should render profile page", async (done) => {
+      const cookie = await getProfileUserCookie();
+      request.get("/profile").set("Cookie", cookie).expect(200, done);
+    });
+  });
+});
