43c1be583d08c70c58b5d1f47c5b8a7e0bdefa2e
bocar <bocaralhassanwane@gmail.com>
2021-05-25 09:28:00 -0400

    Add expressHandlebars to services directory

---
+++ services/expressHandlebars.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const hbs = require("express-handlebars");
+
+const config = {
+  extname: "hbs",
+  defaultLayout: false,
+  layoutsDir: path.join(__dirname, "../views"),
+  partialsDir: path.join(__dirname, "../views/partials"),
+};
+
+const hbsConfig = hbs(config);
+
+module.exports = {
+  hbsConfig,
+};
