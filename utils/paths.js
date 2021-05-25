395905ceb06185375b03fae71f07b5e3069e6074
bocar <bocaralhassanwane@gmail.com>
2021-05-25 08:27:00 -0400

    Add paths to utils directory

---
+++ utils/paths.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+const views = [
+  path.join(__dirname, "../views"),
+  path.join(__dirname, "../views/planets"),
+  path.join(__dirname, "../views/members-area"),
+];
+
+const mainDirectory = path.join(__dirname, "../");
+
+module.exports = {
+  views,
+  mainDirectory,
+};
