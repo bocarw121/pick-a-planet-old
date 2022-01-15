f194ccaa567caefcf1b5d59c82e78e7cd9ea1b24
Bocar Wane <bocaralhassanwane@gmail.com>
2022-01-15 11:50:37 -0500

    Add pm2 and fix server local variable bug

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ const server = http.createServer(app);
 if (NODE_ENV === "production") {
   app.use(enforceHttps);
 }
-const PORT = ENV_PORT || 3000
-
+const PORT = ENV_PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Your server is listening on port ${PORT}`);
