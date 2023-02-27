24d55a2df4c4d65bf2c6e93f7e04d13c2430109e
bocarw121 <bocaralhassanwane@gmail.com>
2023-02-27 18:47:56 -0500

    Add functionality to edit profile

--- routes/members/profile.controller.js
+++ routes/members/profile.controller.js
@@ -1,3 +1,10 @@
+const { StatusCodes } = require('http-status-codes');
+
+// const { updateUserEmail, getUserById } = require('../../../models/user.model');
+const { verifyToken, removeCookie } = require('../../services/security');
+const { validationSchema } = require('../../validation/schemas');
+const { Users } = require('../../db/prisma');
+
 const getProfile = (req, res) => {
   if (req.user) {
     res.render('profile', {
@@ -25,7 +32,95 @@ const getEditProfile = (req, res) => {
   }
 };
 
+const validateUserInformation = (req, res, next) => {
+  const { email } = req.body;
+  const currentEmail = req.user.email;
+
+  try {
+    const validate = validationSchema.updateUserInformation.validate(req.body);
+
+    // const type = value.error.details[0].type;
+    // Were returning json instead of rendering the page with the error message
+    if (req.cookies.userId) {
+      if (validate.error) {
+        console.log('validate.error1');
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: validate.error.details[0].message,
+          success: false,
+        });
+      }
+
+      if (currentEmail === email) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          message: 'New email must be different then current',
+          success: false,
+        });
+      }
+    }
+
+    next();
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { firstName, lastName, email } = req.body;
+  // const currentEmail = req.user.email;
+
+  // console.log('req.body', req.body);
+
+  const decoded = await verifyToken(req);
+
+  const user = await Users.findUnique({
+    where: {
+      id: decoded.id,
+    },
+  });
+
+  if (!user) {
+    removeCookie(res);
+    return res.redirect('login');
+  }
+
+  // update only the fields that are not empty
+
+  if (firstName) {
+    user.firstName = firstName;
+  }
+
+  if (lastName) {
+    user.lastName = lastName;
+  }
+
+  if (email) {
+    user.email = email;
+  }
+
+  console.log('user', user);
+
+  const updatedUser = await Users.update({
+    where: {
+      id: decoded.id,
+    },
+    data: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    },
+  });
+
+  if (updatedUser) {
+    return res.status(StatusCodes.OK).json({
+      message: 'Your information has been updated successfully ',
+      success: true,
+    });
+  }
+};
+
 module.exports = {
   getProfile,
   getEditProfile,
+  updateUser,
+  validateUserInformation,
 };
--- validation/schemas.js
+++ validation/schemas.js
@@ -5,37 +5,68 @@ class ValidationSchemas {
     return joi.object({
       firstName: joi.string().max(40).required(),
       lastName: joi.string().max(40).required(),
-      email: joi.string().email().required(),
-      password: joi.string().min(6).max(40).required(),
-      passwordConfirm: joi.ref('password'),
+      email: joi.string().email().required().messages({
+        'string.empty': 'Email must be a valid email',
+      }),
+      password: joi.string().min(6).max(40).required().messages({
+        'string.empty': 'Password must be at least 6 characters',
+      }),
+      passwordConfirm: joi
+        .string()
+        .required()
+        .valid(joi.ref('password'))
+        .messages({
+          'any.only': 'Passwords do not match',
+          'string.empty': 'Password must be at least 6 characters',
+        }),
     });
   }
 
   static get login() {
     return joi.object({
-      email: joi.string().email().required(),
-      password: joi.string().min(6).max(40).required(),
+      email: joi.string().email().required().messages({
+        'string.empty': 'Email must be a valid email',
+      }),
+      password: joi.string().min(6).max(40).required().messages({
+        'string.empty': 'Password must be at least 6 characters',
+      }),
     });
   }
 
   static get resetPassword() {
     return joi.object({
-      password: joi.string().min(6).max(40).required(),
+      password: joi.string().min(6).max(40).required().messages({
+        'string.empty': 'Password must be at least 6 characters',
+        'ref:password': 'Passwords do not match',
+      }),
       passwordConfirm: joi.ref('password'),
     });
   }
 
   static get email() {
     return joi.object({
-      email: joi.string().email().required(),
+      email: joi.string().email().required().messages({
+        'string.empty': 'Email must be a valid email',
+      }),
     });
   }
 
   static get updateUserInformation() {
     return joi.object({
-      firstName: joi.string().max(40).required(),
-      lastName: joi.string().max(40).required(),
-      email: joi.string().email().required(),
+      firstName: joi.string().max(40).allow(''),
+      lastName: joi.string().max(40).allow(''),
+      email: joi
+        .string()
+        .email()
+        .min(0)
+        .messages({
+          'string.empty': 'Email must be a valid email',
+        })
+        .allow(''),
+      emailConfirm: joi.string().required().valid(joi.ref('email')).messages({
+        'any.only': 'Emails do not match',
+        'string.empty': 'Email must be a valid email',
+      }),
     });
   }
 }
